75e81610cf5c25935221a4e13ab6a98d1d7f5882
atlaska826 <131003995+atlaska826@users.noreply.github.com>
2023-10-10 20:35:40 -0500

    Continued lab 7

--- Lab 7/lab7.py
+++ Lab 7/lab7.py
@@ -16,45 +16,57 @@ Errors needed:
 
 
 def get_min_mpg() -> int:
-    # TODO: Handle exceptions
-    min_mpg = int(input('Enter the minimum mpg ==> '))
-    if 0 < min_mpg <= 100:
-        return min_mpg
-    else:
-        print('FIXME: Wrong number handler')
+    while True:
+        try:
+            min_mpg = int(input('Enter the minimum mpg ==> '))
+            if 0 < min_mpg <= 100:
+                print()
+                return min_mpg
+            else:
+                if min_mpg <= 0:
+                    print('Fuel economy must be greater than 0.\n')
+                else:
+                    print('Fuel economy must be less than or equal to 100.\n')
+        except ValueError:
+            print('You must enter a number for the fuel economy.\n')
 
 
 def get_input_file() -> str:
-    pass
+    while True:
+        input_filename = input('Enter the name of the input vehicle file ==> ')
+        try:
+            with open(input_filename, 'r') as file:
+                print()
+                return input_filename
+        except FileNotFoundError:
+            print(f'Could not open file {input_filename}.\n')
 
 
 def get_output_file() -> str:
-    pass
+    while True:
+        output_filename = input('Enter the name of the file to output to ==> ')
+        try:
+            with open(output_filename, 'w+') as out_file:
+                return output_filename
+        except IOError:
+            print(f'There is an IO Error {output_filename}')
 
 
 """MAIN PROGRAM"""
-# for vehicle in input file >= min_mpg then output to file
+min_mpg = get_min_mpg()
 
-input_file = 'vehicles2.txt'
-output_file = 'output.txt'
-min_mpg = 50
+with open(get_input_file(), 'r') as input_file:
+    out_file = open(get_output_file(), 'w')
+    print()
 
-with open(input_file, 'r') as in_file:
-    # FIXME: Add conditional for input file read
-    out_file = open(output_file, 'w+')
-
-    for line in in_file:
-        lines = in_file.readlines()
+    for line in input_file:
+        lines = input_file.readlines()
         for item in lines:
             item_list = item.split('\t')
-            print(item_list)
+            try:
+                if float(item_list[7]) >= min_mpg:
+                    out_file.write(f'{item_list[0] + " " + item_list[1]:<40}{item_list[2]:<40}{float(item_list[7]):>40.3f}\n')
+            except (ValueError, TypeError):
+                print(f'Could not convert value {item_list[7]} for vehicle {item_list[0]} {item_list[1]} {item_list[2]}')
 
-        try:
-            if float(item_list[7]) >= min_mpg:
-                out_file.write('Hi')
-        except (ValueError, TypeError):  # FIXME: Add exception handler
-            print('ERROR THROWN!')
-
-    # FIXME: Output stuff here
-    # Year, make, model left justified 40 spaces AND mpg right justified 20 spaces
     out_file.close()
