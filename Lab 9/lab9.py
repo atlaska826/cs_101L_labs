de45859bd5e8318a531f97ade834d9de5081bb65
atlaska826 <131003995+atlaska826@users.noreply.github.com>
2023-10-24 14:55:20 -0500

    Finished adding comments to lab9.py

--- Lab 9/lab9.py
+++ Lab 9/lab9.py
@@ -2,24 +2,16 @@ import csv
 
 """ VARIABLES """
 months = {
-    1: 'January',
-    2: 'February',
-    3: 'March',
-    4: 'April',
-    5: 'May',
-    6: 'June',
-    7: 'July',
-    8: 'August',
-    9: 'September',
-    10: 'October',
-    11: 'November',
-    12: 'December'
+    1: 'January', 2: 'February', 3: 'March', 4: 'April',
+    5: 'May', 6: 'June', 7: 'July', 8: 'August',
+    9: 'September', 10: 'October', 11: 'November', 12: 'December'
 }
 
 """ FUNCTIONS """
 
 
 def read_in_file(filename):
+    """Reads file with specific name and returns a list of its contents"""
     input_file_list = []
     with open(filename, 'r', encoding="utf-8") as file:
         reader = csv.reader(file)
@@ -30,12 +22,14 @@ def read_in_file(filename):
 
 
 def month_from_number(num):
+    """Returns name of the month when given a number"""
     if num in months:
         return months[num]
     raise ValueError("Not a valid month!")
 
 
 def create_reported_date_dict(data):
+    """Returns a dictionary of the number of crime occurrences on each specific date"""
     reported_date = {}
     for line in data[1:]:
         if reported_date.get(line[1], 0) == 0:
@@ -46,6 +40,7 @@ def create_reported_date_dict(data):
 
 
 def create_reported_month_dict(data):
+    """Returns a dictionary of the number of crime occurrences during each month"""
     reported_month = {}
     for line in data[1:]:
         date_list = line[1].split("/")
@@ -59,6 +54,7 @@ def create_reported_month_dict(data):
 
 
 def create_offense_dict(data):
+    """Returns a dictionary of the number of occurrences of each specific crime"""
     offense = {}
     for line in data[1:]:
         if offense.get(line[7], 0) == 0:
@@ -69,6 +65,7 @@ def create_offense_dict(data):
 
 
 def create_offense_by_zip(data):
+    """Returns a dictionary of the number of occurrences in each zipcode of each individual crime."""
     offense_zip = {}
     for line in data[1:]:
         offense = line[7]
@@ -94,10 +91,37 @@ if __name__ == "__main__":
         except FileNotFoundError:
             print(f'Could not find the file specified. {user_filename} not found.')
 
-    print(f'\nThe month with the highest # of crimes is {high_month} with {num_per_month} offenses')
-    print(f'The offense with the highest # of crimes is {high_offense} with {num_per_offense} offenses\n')
-
-    date_dict = create_reported_date_dict(data_list)
+    # Initializes variables to store the return value of each dictionary creation function
     month_dict = create_reported_month_dict(data_list)
     offense_dict = create_offense_dict(data_list)
     offense_zip_dict = create_offense_by_zip(data_list)
+
+    # Gets highest crime month and number of crimes committed that month
+    high_month = 0
+    num_per_month = 0
+    for month, count in month_dict.items():
+        if count > num_per_month:
+            high_month = month
+            num_per_month = count
+    print(f'\nThe month with the highest # of crimes is {months[high_month]} with {num_per_month} offenses')
+
+    # Gets highest occurring event and the number of times the crime was committed
+    high_offense = ''
+    num_per_offense = 0
+    for offense, count in offense_dict.items():
+        if count > num_per_offense:
+            high_offense = offense
+            num_per_offense = count
+    print(f'The offense with the highest # of crimes is {high_offense} with {num_per_offense} offenses\n')
+
+    # Gets zip codes for a specific offense
+    crime = input('Enter an offense ==> ')
+    while crime not in offense_zip_dict:
+        print('Not a valid offense. Please try again.')
+        crime = input('Enter an offense ==> ')
+
+    print(f'\n{crime} offenses by Zip Code')
+    print(f'Zip Code{"# Offenses":>22}')
+    print('=' * 30)
+    for zip_code, count in offense_zip_dict[crime].items():
+        print(f'{zip_code:<8}{count:>22}')
