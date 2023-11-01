1cb94203dc59e6f4b1a34d01d132c2756e0a8382
atlaska826 <131003995+atlaska826@users.noreply.github.com>
2023-11-01 13:28:42 -0500

    Continued work on lab 10

---
+++ Lab 10/lab10.py
@@ -0,0 +1,62 @@
+import string
+
+""" FUNCTIONS """
+
+
+def get_file():
+    """Gets a valid file from user input"""
+    while True:
+        filename = input('Enter the name of the file to open ==> ')
+        try:
+            with open(filename, 'r') as test:
+                return filename
+        except FileNotFoundError:
+            print(f'Could not open file {filename}. Please try again.\n')
+
+
+def cleanup_text(text):
+    """Removes all punctuation, whitespace, and any words that are 3 characters or fewer from the file text list"""
+    cleanup_lst = []
+    for line in text:
+        line_lst = [word.strip(string.punctuation).lower() for word in line.split()]
+        for word in line_lst:
+            if len(word) > 3:
+                cleanup_lst.append(word)
+    return cleanup_lst
+
+
+def get_top_ten(words):
+    """Returns a sorted dictionary with the word as the key and the value as the frequency of the word"""
+    count_dict = {}
+    for word in words:
+        count_dict[word] = words.count(word)
+    return dict({w: c for w, c in sorted(count_dict.items(), key=lambda elem: elem[1], reverse=True)})
+
+
+def get_used_once(words):
+    """Returns the number of words that are only used once"""
+    used_once = 0
+    for word in words:
+        if words.count(word) == 1:
+            used_once += 1
+    return used_once
+
+
+""" MAIN PROGRAM """
+with open(get_file(), 'r') as file:
+    word_list = cleanup_text(file.readlines())
+
+print(f'\n\n{"Most Frequently Used Words":^36}')
+print(f'{"#":<12}{"Word":>4}{"Freq.":>20}')
+print('=' * 36)
+
+top_words = get_top_ten(word_list)
+
+num = 1
+for word, count in top_words.items():
+    if num <= 10:
+        print(f'{num:<3}{word:>13}{count:>20}')
+    num += 1
+
+print(f'\nThere are {get_used_once(word_list)} words that only occur once.')
+print(f'There are {len(set(word_list))} unique words in the document.')
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # CS 101L Python Projects
 
 ### Table of contents:
-* Week 1 --- **NO LAB**
+* Week 1 --- No Lab
 * Week 2 --- [Lab 1 - _Python_](Lab%201/lab1.py)
 * Week 3 --- [Lab 2 - _Basic Skills_](Lab%202/lab2.py)
 * Week 4 --- [Lab 3 - _Loops and Conditionals_](Lab%203/lab3.py)
@@ -9,16 +9,20 @@
   * Unit Tests - [Slot_UnitTests.py](Lab%204/Slot_UnitTests.py)
 * Week 6 --- [Lab 5 - _Strings_](Lab%205/lab5.py)
   * Unit Tests - [lab05_tests.py](Lab%205/lab05_tests.py)
-* Week 7 --- [Lab 6 - _EXAM 1_](Exam%201/lab6.py)
+* Week 7 --- [Lab 6 - _EXAM 1_](Lab%206%20(Exam%201)/lab6.py)
 * Week 8 --- [Lab 7 - _Files and Try Except_](Lab%207/lab7.py)
 * Week 9 --- [Lab 8 - _Lists_](Lab%208/lab8.py)
   * Inputs - [lab8_input.txt](Lab%208/lab8_input.txt)
 * Week 10 --- [Lab 9 - _Dictionaries_]()
   * Inputs - [lab9_input.txt](Lab%209/lab9_input.txt)
   * Unit Tests - [CrimeDataTests.py](Lab%209/CrimeDataTests.py)
-* Week 11 --- [Lab 10 - _Dictionaries and Sets_](Lab%2010/lab10.py)
-  * Inputs --- [lab9_input.txt](Lab%2010/lab10_input.txt)
-* Week 12 --- **TBD**
+* Week 11 --- [Lab 10 - _Sets_](Lab%2010/lab10.py)
+  * Inputs --- [lab10_input.txt](Lab%2010/lab10_input.txt)
+* Week 12 --- [Lab 11 - _EXAM 2_]()
+  * Inputs --- [lab11_input.txt]()
 * Week 13 --- [Lab 12 - _Classes_]()
+  * Inputs - [lab12_input.txt]()
 * Week 14 --- [Lab 13 - _Inheritance_]()
+  * Inputs - [lab13_input.txt]()
 * Week 15 --- [Lab 14 - _Creating Unit tests_]()
+  * Inputs - [lab14_input.txt]()
