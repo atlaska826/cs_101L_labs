66f4a314d987c964c8e08db8df03fbd0d41692f0
atlaska826 <131003995+atlaska826@users.noreply.github.com>
2023-11-28 17:40:49 -0600

    Finished Lab 13

--- Lab 13/lab13.py
+++ Lab 13/lab13.py
@@ -1,7 +1,135 @@
 import turtle
+import os
+from PIL import Image
 
+cwd = os.getcwd()
+
+""" CLASSES """
+class Point(object):
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def draw(self):
+        turtle.penup()
+        turtle.goto(self.x, self.y)
+        turtle.pendown()
+        turtle.color(self.color)
+        turtle.setheading(0)
+        self.draw_action()
+
+    def draw_action(self):
+        turtle.dot()
+
+
+class Box(Point):
+    def __init__(self, x1, y1, width, height, color):
+        super().__init__(x1, y1, color)
+        self.width = width
+        self.height = height
+
+    def draw_action(self):
+        turtle.forward(self.width)
+        turtle.right(90)
+        turtle.forward(self.height)
+        turtle.right(90)
+        turtle.forward(self.width)
+        turtle.right(90)
+        turtle.forward(self.height)
+
+
+class BoxFilled(Box):
+    def __init__(self, x1, y1, width, height, color, fillcolor):
+        super().__init__(x1, y1, width, height, color)
+        self.fillcolor = fillcolor
+
+    def draw_action(self):
+        turtle.fillcolor(self.fillcolor)
+        turtle.begin_fill()
+        Box.draw_action(self)
+        turtle.end_fill()
+
+
+class Circle(Point):
+    def __init__(self, x1, y1, radius, color):
+        super().__init__(x1, y1, color)
+        self.radius = radius
+
+    def draw_action(self):
+        turtle.circle(self.radius)
+
+
+class CircleFilled(Circle):
+    def __init__(self, x1, y1, radius, color, fillcolor):
+        super().__init__(x1, y1, radius, color)
+        self.fillcolor = fillcolor
+
+    def draw_action(self):
+        turtle.fillcolor(self.fillcolor)
+        turtle.begin_fill()
+        Circle.draw_action(self)
+        turtle.end_fill()
+
+
+""" EXPORT IMAGE """
+def export_image(image_name):
+    """Creates an image of the current turtle screen and adds them to an Images folder"""
+    # Figure out how to save certain frames to a folder of images, so you don't have to ss each one
+    if not os.path.exists(cwd + '/Images/'):
+        os.mkdir(cwd + '/Images/')
+
+    filename = 'lab13_img_' + str(image_name)
+    screen = turtle.getscreen()
+    screen.getcanvas().postscript(file=filename + '.eps')
+
+    with Image.open(filename + '.eps') as image:
+        image.convert()
+        image.save(f'{cwd}/Images/{filename + ".jpg"}')
+
+    os.remove(f'{cwd}/{filename + ".eps"}')
+    turtle.clear()
+
+
+""" MAIN PROGRAM """
+# Turtle
 turtle.forward(100)
 turtle.right(90)
 turtle.forward(50)
+export_image('Turtle')
 
-turtle.Screen().exitonclick()
+# Turtle Filled
+turtle.fillcolor('red')
+turtle.begin_fill()
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.end_fill()
+export_image('TurtleFilled')
+
+# Point
+p = Point(-100, 100, "blue")
+p.draw()
+export_image('Point')
+
+# Box
+b = Box(-100, 100, 50, 20, 'blue')
+b.draw()
+export_image('Box')
+
+# BoxFilled
+b = BoxFilled(1, 2, 100, 200, 'red', 'blue')
+b.draw()
+export_image('BoxFilled')
+
+# Circle
+c = Circle(-100, 100, 50, 'blue')
+c.draw()
+export_image('Circle')
+
+# Circle Filled
+c = CircleFilled(1, 2, 100, 'red', 'blue')
+c.draw()
+export_image('CircleFilled')
